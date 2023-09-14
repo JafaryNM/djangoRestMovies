7b37127579ea7450e3a1b31d7a89ab6f2ca0ad86
Baraka Kiranga <jaffarymdegela.com>
2023-09-14 19:40:49 +0300

    loginPermenentDjango

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('movies/',include('watchlist_app.urls')),
-    path('apis/', include('api.urls'))
+    path('apis/', include('api.urls')),
+    path('api-auth/',include('rest_framework.urls'))
 ]
